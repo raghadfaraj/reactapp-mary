eeb3a83e18f7d63427d0cc827086ee5f4b798466
Nafeel483 <m.nafeelrasheed@gmail.com>
2023-10-24 05:35:15 +0500

    resolved backend and frontend issues

--- Bakend/index.js
+++ Bakend/index.js
@@ -17,172 +17,124 @@ const users = [
 const items = [
   {
     id: "id123",
-    price: "$14.8",
-    imgURL:
-      "https://ii1.pepperfry.com/media/catalog/product/m/o/568x625/modern-chaise-lounger-in-grey-colour-by-dreamzz-furniture-modern-chaise-lounger-in-grey-colour-by-dr-tmnirx.jpg",
-    text: "Pioneer LHS Chaise Lounger in Grey Colour",
+    price: "$25.8",
+    category: "Walking",
+    imgURL: `https://i.ibb.co/mc61LKy/Berghaus-womens-waterproof-on-the-hill-a.webp`,
+    text: "Bumber Jacket",
   },
   {
     id: "id124",
-    price: "$14.8",
-    imgURL:
-      "https://www.precedent-furniture.com/sites/precedent-furniture.com/files/styles/header_slideshow/public/3360_SL%20CR.jpg?itok=3Ltk6red",
-    text: "Precedant Furniture",
+    price: "$6.8",
+    category: "Walking",
+    imgURL: `https://ibb.co/Kz6nFsw`,
+    text: "Local Linen",
   },
   {
     id: "id125",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/leverette-fabric-queen-upholstered-platform-bed-1594829293.jpg",
-    text: "Leverette Upholstered Platform Bed",
+    category: "Walking",
+    imgURL: `https://ibb.co/YyTymd2`,
+    text: "Raining Free",
   },
   {
     id: "id126",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/briget-side-table-1582143245.jpg?crop=1.00xw:0.770xh;0,0.129xh&resize=768:*",
-    text: "Briget Accent Table",
+    price: "$17.8",
+    category: "Walking",
+    imgURL: `https://ibb.co/pJWTX1v`,
+    text: "Jackets",
   },
   {
     id: "id127",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rivet-emerly-media-console-1610578756.jpg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Rivet Emerly Media Console",
+    price: "$33.8",
+    category: "Walking",
+    imgURL: `https://ibb.co/pJWTX1v`,
+    text: "Jackets",
   },
   {
     id: "id128",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/drew-barrymore-flower-home-petal-chair-1594829759.jpeg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Drew Barrymore Flower Home Accent Chair",
+    price: "$8.8",
+    category: "Running",
+    imgURL: `https://ibb.co/tZnYMh2`,
+    text: "Jackets",
   },
   {
     id: "id129",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/goodee-ecobirdy-charlie-chairs-1594834221.jpg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Ecobirdy Charlie Chair",
+    category: "Running",
+    imgURL: `https://ibb.co/qMD70bR`,
+    text: "Jackets",
   },
   {
     id: "id130",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hailey-sofa-1571430947.jpg?crop=0.481xw:0.722xh;0.252xw,0.173xh&resize=768:*",
-    text: "Hailey Sofa",
+    category: "Running",
+    imgURL: `https://ibb.co/n8wBWC7`,
+    text: "Jackets",
   },
   {
     id: "id131",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/archer-home-designs-dining-table-1594830125.jpg?crop=0.657xw:1.00xh;0.0986xw,0&resize=768:*",
-    text: "Farmhouse Dining Table",
+    category: "Running",
+    imgURL: `https://ibb.co/t8NKnX7`,
+    text: "Jackets",
   },
   {
     id: "id132",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/evelyn-coffee-table-1610578857.jpeg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Evelyn Coffee Table",
+    category: "Running",
+    imgURL: `https://ibb.co/MVWwZ2q`,
+    text: "Jackets",
   },
   {
     id: "id133",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/burrow-nomad-sofa-1594837995.jpg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Slope Nomad Leather Sofa",
+    category: "Running",
+    imgURL: `https://ibb.co/qDkT66Q`,
+    text: "Jackets",
   },
   {
     id: "id134",
     price: "$14.8",
-    imgURL:
-      "https://apicms.thestar.com.my/uploads/images/2020/02/21/570850.jpg",
-    text: "Chair and Table",
+    category: "Running",
+    imgURL: `https://ibb.co/gMBMbBG`,
+    text: "Jackets",
   },
   {
     id: "id223",
     price: "$14.8",
-    imgURL:
-      "https://ii1.pepperfry.com/media/catalog/product/m/o/568x625/modern-chaise-lounger-in-grey-colour-by-dreamzz-furniture-modern-chaise-lounger-in-grey-colour-by-dr-tmnirx.jpg",
-    text: "Pioneer LHS Chaise Lounger in Grey Colour",
+    category: "General",
+    imgURL: `https://ibb.co/zG7W89j`,
+    text: "Jackets",
   },
   {
     id: "id224",
     price: "$14.8",
-    imgURL:
-      "https://www.precedent-furniture.com/sites/precedent-furniture.com/files/styles/header_slideshow/public/3360_SL%20CR.jpg?itok=3Ltk6red",
-    text: "Precedant Furniture",
+    category: "General",
+    imgURL: `https://ibb.co/w0qmptb`,
+    text: "Jackets",
   },
   {
     id: "id225",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/leverette-fabric-queen-upholstered-platform-bed-1594829293.jpg",
-    text: "Leverette Upholstered Platform Bed",
+    category: "General",
+    imgURL: `https://ibb.co/V3BnjcM`,
+    text: "Jackets",
   },
   {
     id: "id226",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/briget-side-table-1582143245.jpg?crop=1.00xw:0.770xh;0,0.129xh&resize=768:*",
-    text: "Briget Accent Table",
+    category: "General",
+    imgURL: `https://ibb.co/HH13CDC`,
+    text: "Tour Jackets",
   },
   {
     id: "id227",
     price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rivet-emerly-media-console-1610578756.jpg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Rivet Emerly Media Console",
+    category: "General",
+    imgURL: `https://ibb.co/1Q2pS7X`,
+    text: "Snow Jackets",
   },
-  {
-    id: "id228",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/drew-barrymore-flower-home-petal-chair-1594829759.jpeg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Drew Barrymore Flower Home Accent Chair",
-  },
-  {
-    id: "id229",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/goodee-ecobirdy-charlie-chairs-1594834221.jpg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Ecobirdy Charlie Chair",
-  },
-  {
-    id: "id230",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hailey-sofa-1571430947.jpg?crop=0.481xw:0.722xh;0.252xw,0.173xh&resize=768:*",
-    text: "Hailey Sofa",
-  },
-  {
-    id: "id231",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/archer-home-designs-dining-table-1594830125.jpg?crop=0.657xw:1.00xh;0.0986xw,0&resize=768:*",
-    text: "Farmhouse Dining Table",
-  },
-  {
-    id: "id232",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/evelyn-coffee-table-1610578857.jpeg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Evelyn Coffee Table",
-  },
-  {
-    id: "id233",
-    price: "$14.8",
-    imgURL:
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/burrow-nomad-sofa-1594837995.jpg?crop=1xw:1xh;center,top&resize=768:*",
-    text: "Slope Nomad Leather Sofa",
-  },
-  {
-    id: "id234",
-    price: "$14.8",
-    imgURL:
-      "https://apicms.thestar.com.my/uploads/images/2020/02/21/570850.jpg",
-    text: "Chair and Table",
-  },
+ 
 ];
 
 // app.use(bodyParser());
